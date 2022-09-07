6a53b9e9bc52fc289836cd1e5e860036d4dc741c
kira.nagayama <kira20011025@gmail.com>
2022-09-07 14:51:10 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
   def index
-   @pagy, @users = pagy(User.order(id: :desc), items: 25)
+    @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
 
   def show
-   @user = User.find(params[:id])
-   @pagy, @microposts = pagy(@user.microposts.order(id: :desc))
-   counts(@user)
+    @user = User.find(params[:id])
+    @pagy, @microposts = pagy(@user.microposts.order(id: :desc))
+    counts(@user)
   end
 
   def new
-   @user = User.new
+    @user = User.new
   end
 
   def create
@@ -27,8 +27,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @user = User.find(params[:id])
+    @pagy, @followings = pagy(@user.followings)
+    counts(@user)
+  end
 
- private
+  def followers
+    @user = User.find(params[:id])
+    @pagy, @followers = pagy(@user.followers)
+    counts(@user)
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
